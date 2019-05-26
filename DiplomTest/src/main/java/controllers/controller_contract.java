2703e98d4fcd431e9e078269bc323e36f671de8d
SoftBishop <reaper057@gmail.com>
2019-05-27 03:22:02 +0700

    fill contract tableview

--- DiplomTest/src/main/java/controllers/controller_contract.java
+++ DiplomTest/src/main/java/controllers/controller_contract.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import SqlClasses.ConnectionPool;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,42 +8,96 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import modeltables.tableview_contract;
+
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
 public class controller_contract implements Initializable {
 
-    private ObservableList<ObservableList> data;
-    @FXML
-    private TableView<?> contractTable;
+
 
     @FXML
-    private TableColumn<?, ?> numberContractColumnTable;
+    private TableView<tableview_contract> contractTable;
 
     @FXML
-    private TableColumn<?, ?> typeContractColTable;
+    private TableColumn<tableview_contract, String> numberContractColumnTable;
 
     @FXML
-    private TableColumn<?, ?> dateCreationColTable;
+    private TableColumn<tableview_contract, String> typeContractColTable;
 
     @FXML
-    private TableColumn<?, ?> dateofExecColTable;
+    private TableColumn<tableview_contract, String> dateCreationColTable;
 
     @FXML
-    private TableColumn<?, ?> dateFinishedColTable;
+    private TableColumn<tableview_contract, String> dateExecColTable;
 
     @FXML
-    private TableColumn<?, ?> clinetColTable;
+    private TableColumn<tableview_contract, String> dateFinishedColTable;
 
     @FXML
-    private TableColumn<?, ?> organizationColTable;
+    private TableColumn<tableview_contract, String> clientColTable;
 
     @FXML
-    private TableColumn<?, ?> employerColTable;
+    private TableColumn<tableview_contract, String> organizationColTable;
+
+    @FXML
+    private TableColumn<tableview_contract, String> employerColTable;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        FillContractTableView();
+    }
 
+    private ObservableList<tableview_contract> olist = FXCollections.observableArrayList();
+    private void FillContractTableView() {
+        try
+        {
+            Connection connection;
+
+            connection = ConnectionPool.getDataSource().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT contracts.dogovorid, contracts.DateOfCreationContract,contracts.DateExecution,\n" +
+                    "contracts.DateExpire, contracts.Price,\n" +
+                    "typecontracts.NameTypeContract,\n" +
+                    "clients.FIO as clientfio,\n" +
+                    "organizations.NameOrganization,\n" +
+                    "Employers.FIO as employerfio\n" +
+                    "FROM contracts join typecontracts \n" +
+                    "on contracts.TypeContractsTypeContractID=typecontracts.typecontractid\n" +
+                    "join clients on contracts.Clients = clients.Client_ID\n" +
+                    "join Organizations on contracts.Organizations = Organizations.organizationid\n" +
+                    "join Employers on  contracts.Employers = Employers.EmployerID");
+
+            ResultSet rs = preparedStatement.executeQuery();
+                while(rs.next())
+                {
+                    olist.add(new tableview_contract(rs.getString("dogovorid"),
+                                                    rs.getString("NameTypeContract"),
+                                                    rs.getString("DateOfCreationContract"),
+                                                    rs.getString("DateExecution"),
+                                                    rs.getString("DateExpire"),
+                                                    rs.getString("clientfio"),
+                                                    rs.getString("NameOrganization"),
+                                                    rs.getString("employerfio")));
+                }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+        numberContractColumnTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("contractID"));
+        typeContractColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("contractType"));
+        dateCreationColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("contractDateOfCreation"));
+        dateExecColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("contractDateExec"));
+        dateFinishedColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("contractDateFinished"));
+        clientColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("nameClient"));
+        organizationColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("nameOrganization"));
+        employerColTable.setCellValueFactory(new PropertyValueFactory<tableview_contract,String>("nameEmployer"));
+
+        contractTable.setItems(olist);
     }
 }
---
+++ DiplomTest/src/main/java/modeltables/tableview_contract.java
@@ -0,0 +1,83 @@
+package modeltables;
+
+public class tableview_contract {
+    String contractID, contractType,contractDateOfCreation,
+            contractDateExec, contractDateFinished ,nameClient, nameOrganization, nameEmployer;
+
+    public tableview_contract(String contractID, String contractType, String contractDateOfCreation,
+                              String contractDateExec, String contractDateFinished, String nameClient,
+                              String nameOrganization, String nameEmployer) {
+        this.contractID = contractID;
+        this.contractType = contractType;
+        this.contractDateOfCreation = contractDateOfCreation;
+        this.contractDateExec = contractDateExec;
+        this.contractDateFinished = contractDateFinished;
+        this.nameClient = nameClient;
+        this.nameOrganization = nameOrganization;
+        this.nameEmployer = nameEmployer;
+    }
+
+    public String getContractID() {
+        return contractID;
+    }
+
+    public void setContractID(String contractID) {
+        this.contractID = contractID;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getContractDateOfCreation() {
+        return contractDateOfCreation;
+    }
+
+    public void setContractDateOfCreation(String contractDateOfCreation) {
+        this.contractDateOfCreation = contractDateOfCreation;
+    }
+
+    public String getContractDateExec() {
+        return contractDateExec;
+    }
+
+    public void setContractDateExec(String contractDateExec) {
+        this.contractDateExec = contractDateExec;
+    }
+
+    public String getContractDateFinished() {
+        return contractDateFinished;
+    }
+
+    public void setContractDateFinished(String contractDateFinished) {
+        this.contractDateFinished = contractDateFinished;
+    }
+
+    public String getNameClient() {
+        return nameClient;
+    }
+
+    public void setNameClient(String nameClient) {
+        this.nameClient = nameClient;
+    }
+
+    public String getNameOrganization() {
+        return nameOrganization;
+    }
+
+    public void setNameOrganization(String nameOrganization) {
+        this.nameOrganization = nameOrganization;
+    }
+
+    public String getNameEmployer() {
+        return nameEmployer;
+    }
+
+    public void setNameEmployer(String nameEmployer) {
+        this.nameEmployer = nameEmployer;
+    }
+}
