129ee5c2de7d4b3831e8ac3f7b3ad23005ae64b7
SoftBishop <reaper057@gmail.com>
2019-05-29 02:56:13 +0700

    implemeted view estimate

--- DiplomTest/src/main/java/controllers/controller_scopeofwork_estimate.java
+++ DiplomTest/src/main/java/controllers/controller_scopeofwork_estimate.java
@@ -1,4 +1,170 @@
 package controllers;
 
-public class controller_scopeofwork_estimate {
+import SqlClasses.ConnectionPool;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import modeltables.tableview_scopeofwork;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+public class controller_scopeofwork_estimate implements Initializable {
+
+    @FXML
+    private TableView<tableview_scopeofwork> scopeOfWorkTableView;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> nameWorkCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> quantityCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> nameTypeWorkCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> measureUnitsCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> estimateIDCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> employerFIOCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> dateExecCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> priceOfWorkCol;
+
+    @FXML
+    private TableColumn<tableview_scopeofwork, String> placmentIdCol;
+
+    @FXML
+    private TextField nameWorkTextField;
+
+    @FXML
+    private TextField quantityTextField;
+
+    @FXML
+    private ComboBox<?> measureUnitsComboBox;
+
+    @FXML
+    private ComboBox<?> typeWorkCombobBox;
+
+    @FXML
+    private ComboBox<?> estimateIDComboBox;
+
+    @FXML
+    private ComboBox<?> employerComboBox;
+
+    @FXML
+    private DatePicker dateExecDatePicker;
+
+    @FXML
+    private Button addWorkButton;
+
+    @FXML
+    private Button editWorkButton;
+
+    @FXML
+    private Button deleteWorkButton;
+
+    @FXML
+    private Menu typeWorksMenuButton;
+
+    @FXML
+    private Menu employerMenuButton;
+
+    @FXML
+    private Menu measureUnitsMenuButoon;
+
+    @FXML
+    private ComboBox<?> placementComboBox;
+
+    @FXML
+    private TextField priceTextField;
+
+    @FXML
+    void AddWork(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteWork(ActionEvent event) {
+
+    }
+
+    private ObservableList<tableview_scopeofwork> olist = FXCollections.observableArrayList();
+    private void FillEstimateTable()
+    {
+        try
+        {
+            Connection connection;
+            connection = ConnectionPool.getDataSource().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "SELECT scopeofworkestimates.namework AS NAMEWORK, scopeofworkestimates.quantity AS QUANTITY,\n" +
+                    "typeworks.typeworkname, measureunits.namemeasureunit AS MEASURE, estimates.estimateid, " +
+                    "employers.fio AS EMPFIO,\n" +
+                    "scopeofworkestimates.dateexecution AS DATEEXECUTION ,\n" +
+                    "scopeofworkestimates.price AS price, placement.placementid AS placementid\n" +
+                    "FROM scopeofworkestimates \n" +
+                    "JOIN typeworks ON typeworks.typeworkid = scopeofworkestimates.typeworks\n" +
+                    "JOIN measureunits ON scopeofworkestimates.measureunits = measureunits.mesureunitid\n" +
+                    "JOIN estimates ON scopeofworkestimates.estimates = estimates.estimateid\n" +
+                    "JOIN employers ON employers.employerid = scopeofworkestimates.employers\n" +
+                    "JOIN placement ON placement.placementid = scopeofworkestimates.placementplacementid");
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                olist.add(new tableview_scopeofwork(rs.getString("NAMEWORK"),
+                        rs.getString("QUANTITY"),
+                        rs.getString("typeworkname"),
+                        rs.getString("MEASURE"),
+                        rs.getString("estimateid"),
+                        rs.getString("EMPFIO"),
+                        rs.getString("DATEEXECUTION"),
+                        rs.getString("price"),
+                        rs.getString("placementid")
+                        ));
+            }
+            rs.close();
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+        nameWorkCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("nameWork"));
+        quantityCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("quantity"));
+        nameTypeWorkCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("nameTypeWork"));
+        measureUnitsCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("measureUnit"));
+        estimateIDCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("estimateId"));
+        employerFIOCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("employerFIO"));
+        dateExecCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("dateExec"));
+        priceOfWorkCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("price"));
+        placmentIdCol.setCellValueFactory(new PropertyValueFactory<tableview_scopeofwork,String>("placementId"));
+
+
+        scopeOfWorkTableView.setItems(olist);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FillEstimateTable();
+    }
 }
---
+++ DiplomTest/src/main/java/modeltables/tableview_scopeofwork.java
@@ -0,0 +1,91 @@
+package modeltables;
+
+public class tableview_scopeofwork {
+    String nameWork,quantity, nameTypeWork, measureUnit, estimateId,employerFIO,dateExec,price,placementId;
+
+    public tableview_scopeofwork(String nameWork, String quantity, String nameTypeWork, String measureUnit,
+                                 String estimateId, String employerFIO, String dateExec,
+                                 String price, String placementId) {
+        this.nameWork = nameWork;
+        this.quantity = quantity;
+        this.nameTypeWork = nameTypeWork;
+        this.measureUnit = measureUnit;
+        this.estimateId = estimateId;
+        this.employerFIO = employerFIO;
+        this.dateExec = dateExec;
+        this.price = price;
+        this.placementId = placementId;
+    }
+
+    public String getNameWork() {
+        return nameWork;
+    }
+
+    public void setNameWork(String nameWork) {
+        this.nameWork = nameWork;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNameTypeWork() {
+        return nameTypeWork;
+    }
+
+    public void setNameTypeWork(String nameTypeWork) {
+        this.nameTypeWork = nameTypeWork;
+    }
+
+    public String getMeasureUnit() {
+        return measureUnit;
+    }
+
+    public void setMeasureUnit(String measureUnit) {
+        this.measureUnit = measureUnit;
+    }
+
+    public String getEstimateId() {
+        return estimateId;
+    }
+
+    public void setEstimateId(String estimateId) {
+        this.estimateId = estimateId;
+    }
+
+    public String getEmployerFIO() {
+        return employerFIO;
+    }
+
+    public void setEmployerFIO(String employerFIO) {
+        this.employerFIO = employerFIO;
+    }
+
+    public String getDateExec() {
+        return dateExec;
+    }
+
+    public void setDateExec(String dateExec) {
+        this.dateExec = dateExec;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPlacementId() {
+        return placementId;
+    }
+
+    public void setPlacementId(String placementId) {
+        this.placementId = placementId;
+    }
+}
